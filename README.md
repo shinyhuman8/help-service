ea6737cab439943de1233882abc5a13a8ab174a9
Sergey <serega.sementsoff@yandex.ru>
2024-02-02 19:44:24 +0300

    Добавлена поддержка конкурентного доступа к ресурсам

---
+++ README.md
@@ -0,0 +1,13 @@
+## Сервис психологичечкой поддержки.
+
+### Запуск проекта в Docker
+
+1. mvn clean install
+2. docker-compose up
+3. Приложение работает по http://localhost:8080
+
+### API
+
+get /help-service/v1/support
+
+post /help-service/v1/support
--- src/main/java/org/example/repositories/MotivationRepository.java
+++ src/main/java/org/example/repositories/MotivationRepository.java
@@ -1,16 +1,14 @@
 package org.example.repositories;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class MotivationRepository {
-    private static final Map<Integer, String> PHRASE_REPOSITORY = new HashMap<>();
-
-    private static int PHRASE_COUNT;
+    private static final ConcurrentMap<UUID, String> PHRASE_REPOSITORY = new ConcurrentHashMap<>();
 
     public void save(String phrase) {
-        PHRASE_REPOSITORY.put(++PHRASE_COUNT, phrase);
+        PHRASE_REPOSITORY.put(UUID.randomUUID(), phrase);
         System.out.println("Phrase " + phrase + " successfully save");
     }
 
@@ -18,12 +16,10 @@ public class MotivationRepository {
         if (PHRASE_REPOSITORY.isEmpty())
             return "Sorry, but i don't have any phrases.\n" +
                     "Please save phrase";
-        Random random = new Random();
-        int phrase_id = random.nextInt(1, PHRASE_COUNT+1);
-        return PHRASE_REPOSITORY.get(phrase_id);
-    }
-
-    public static int getPhraseCount() {
-        return PHRASE_COUNT;
+        int randomIndex = new Random().nextInt(PHRASE_REPOSITORY.size());
+        return PHRASE_REPOSITORY.values().stream()
+                .skip(randomIndex)
+                .findFirst()
+                .orElse(null);
     }
 }
