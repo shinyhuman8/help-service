a29de12f6a475d0d031d0c9c78ef8b9a0bcc4016
Sergey <serega.sementsoff@yandex.ru>
2024-02-10 18:33:56 +0300

    Добавлена поддержка DTO

--- src/main/java/org/example/repositories/MotivationRepository.java
+++ src/main/java/org/example/repositories/MotivationRepository.java
@@ -1,25 +1,12 @@
 package org.example.repositories;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import org.example.annotations.Logged;
+import org.example.models.Phrase;
 
-public class MotivationRepository {
-    private static final ConcurrentMap<UUID, String> PHRASE_REPOSITORY = new ConcurrentHashMap<>();
+public interface MotivationRepository {
+    @Logged
+    void save(Phrase phrase);
 
-    public void save(String phrase) {
-        PHRASE_REPOSITORY.put(UUID.randomUUID(), phrase);
-        System.out.println("Phrase " + phrase + " successfully save");
-    }
-
-    public String show() {
-        if (PHRASE_REPOSITORY.isEmpty())
-            return "Sorry, but i don't have any phrases.\n" +
-                    "Please save phrase";
-        int randomIndex = new Random().nextInt(PHRASE_REPOSITORY.size());
-        return PHRASE_REPOSITORY.values().stream()
-                .skip(randomIndex)
-                .findFirst()
-                .orElse(null);
-    }
+    @Logged
+    Phrase show();
 }
