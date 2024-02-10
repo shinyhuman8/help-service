0f4090ab965ffcaf523535babfdd46b5bc93db97
Sergey <serega.sementsoff@yandex.ru>
2024-02-10 18:30:49 +0300

    Добавлено проксирование методов

---
+++ src/main/java/org/example/LoggingInvocationHandler.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.annotations.Logged;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LoggingInvocationHandler implements InvocationHandler {
+    private final Object target;
+
+    public LoggingInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (method.isAnnotationPresent(Logged.class)) {
+            System.out.println("start " + method.getName());
+            final var result = method.invoke(target, args);
+            System.out.println("finished " + method.getName());
+            return result;
+        } else {
+            return method.invoke(target, args);
+        }
+    }
+}
